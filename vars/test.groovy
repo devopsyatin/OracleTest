98b5beb890ae08df5ab193d28a315d207a146a1f
devopsyatin <sawant.yatin@yahoo.in>
2021-03-09 00:03:57 +0530

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,6 @@
 
-env.currentworkdir = pwd()
-println "${env.currentworkdir}"
+//env.currentworkdir = pwd()
+//println "${env.currentworkdir}"
 String objectlist = "${env.currentworkdir}/objectlist.txt"
 
 println "${objectlist}"
