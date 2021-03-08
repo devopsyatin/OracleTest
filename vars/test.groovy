e278af1bd0c071ef9abf906623b17dfd87958db3
devopsyatin <sawant.yatin@yahoo.in>
2021-03-09 00:50:06 +0530

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,14 @@
 
 //env.currentworkdir = pwd()
 //println "${env.currentworkdir}"
-String objectlist = new File ('C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt').text
-def objectslist_list  = objectlist.collect { it }
-println "${objectslist_list}"
+//String objectlist = new File ('C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt').text
+//def objectslist_list  = objectlist.collect { it }
+//println "${objectslist_list}"
+
+String objectlist = "C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt"
+File objectlistfile = new File (objectlist)
+def objectslist_list  = objectlistfile.collect { it }
+
 
 for(String j in objectslist_list) {
 
