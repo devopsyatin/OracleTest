c5ef96abb341b1f5efaa26092e952f21febbe5e4
devopsyatin <sawant.yatin@yahoo.in>
2021-03-09 00:19:39 +0530

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,5 @@
 
 //env.currentworkdir = pwd()
 //println "${env.currentworkdir}"
-String objectlist = "C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt"
-objectlistfile = new File (objectlist).text
+String objectlist = new File ('C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt').text
 println "${objectlist}"
