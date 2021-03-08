3cfe2b6e8b84d5d902a855406fc7676151ce9a70
devopsyatin <sawant.yatin@yahoo.in>
2021-03-09 00:51:47 +0530

    .

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,11 +11,13 @@ def objectslist_list  = objectlistfile.collect { it }
 
 
 for(String j in objectslist_list) {
-
+println "${j}"
 File f = new File(j)
 def objectnameext = f.getName()
 def extension = objectnameext.substring(objectnameext.lastIndexOf('.') + 1)
 println "${extension}"
+
+while 
 
 }
     
