71de00130c39ade5f8c144109c310473a3964b07
devopsyatin <sawant.yatin@yahoo.in>
2021-03-09 00:26:37 +0530

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,3 +3,10 @@
 //println "${env.currentworkdir}"
 String objectlist = new File ('C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt').text
 println "${objectlist}"
+
+for(String j in objectslist) {
+
+def extension = j.substring(objectnameext.lastIndexOf('.') + 1))
+println "${extension}"
+}
+    
