ceeb1d35a3ee31c3f100d8b348a4ca565ed66be8
devopsyatin <sawant.yatin@yahoo.in>
2021-03-10 18:04:48 +0530

    test

--- vars/initialprocess.groovy
+++ vars/initialprocess.groovy
@@ -4,100 +4,94 @@ import groovy.xml.XmlUtil
 def call () {
 //script {
         withCredentials([usernamePassword(credentialsId: 'Reports-BIP-dev-creds', passwordVariable: 'testpass', usernameVariable: 'testuser')]) {
-             //checkout scm
-             //objectList = readTrusted("objectlist.txt")
-             //println "${objectList}"
-             env.currentworkdir = pwd()
-             println "${env.currentworkdir}"
+            //checkout scm
+            //objectList = readTrusted("objectlist.txt")
+            //println "${objectList}"
+            env.currentworkdir = pwd()
+            println "${env.currentworkdir}"
 
-             // Reading the objectlist.txt for no of objects
-             String objectlist_reports = "${env.currentworkdir}/objectlist_reports.txt"
-             File objectlist_reportsfile = new File (objectlist_reports)
-             def objectslistreports_list  = objectlist_reportsfile.collect { it }
+            // Reading the objectlist.txt for no of objects
+            String objectlist_reports = "${env.currentworkdir}/objectlist_reports.txt"
+            File objectlist_reportsfile = new File (objectlist_reports)
+            def objectslistreports_list  = objectlist_reportsfile.collect { it }
 
-             println "===================================================================================="
-             println "Start the Processing of objects in FIFO manner"
-             println "===================================================================================="
+            println "===================================================================================="
+            println "Start the Processing of objects in FIFO manner"
+            println "===================================================================================="
 
-             // Loop for processing the objects in a FIFO manner
-             for(String i in objectslistreports_list) {
-             println(i);
-             //initialprocess()
-             //objectprocess()
+            // Loop for processing the objects in a FIFO manner
+            for(String i in objectslistreports_list) {
+            println(i);
+            //initialprocess()
+            //objectprocess()
 
+            println "===================================================================================="
+            println "Processing the Object ${i}"
+            println "===================================================================================="
+            def objectPath = "${i}"
+            println "Object Path of File : ${objectPath}"
 
-             println "===================================================================================="
-             println "Processing the Object ${i}"
-             println "===================================================================================="
-             def objectPath = "${i}"
-             println "Object Path of File : ${objectPath}"
+            //Objectname with extension
+            File f = new File(objectPath)
+            def objectnameext = f.getName()
+            println "Object name with Ext : ${objectnameext}"
 
-            // //Objectname with extension
-             File f = new File(objectPath)
-             def objectnameext = f.getName()
-             println "Object name with Ext : ${objectnameext}"
+            //Objectname without extension
+            def objectname = objectnameext.take(objectnameext.lastIndexOf('.'))
+            println "Object name : ${objectname}"
 
-             //Objectname without extension
-             def objectname = objectnameext.take(objectnameext.lastIndexOf('.'))
-             println "Object name : ${objectname}"
+            def extFile = objectnameext.substring(objectnameext.lastIndexOf('.') + 1)
+            println "Object ext : ${extFile}"
 
-             def extFile = objectnameext.substring(objectnameext.lastIndexOf('.') + 1)
-             println "Object ext : ${extFile}"
+            //Full Path of File
+            //def fullPath = "${currentDir}/${i}"
+            //println "Full Path of File : ${fullPath}"
 
-             //Full Path of File
-             //def fullPath = "${currentDir}/${i}"
-             //println "Full Path of File : ${fullPath}"
-
-             //Create a Temp Directory
-             def tempDir = new File("${env.currentworkdir}/Temp-Directory")
-             tempDir.mkdir()
-             //println "${tempDir}"
+            //Create a Temp Directory
+            def tempDir = new File("${env.currentworkdir}/Temp-Directory")
+            tempDir.mkdir()
+            //println "${tempDir}"
      
-             println "Converting the object to ${objectname}.zip"
-             def dst = new File("${tempDir}/${objectname}.zip") 
-             def src = new File("${env.currentworkdir}/${objectPath}")
-             dst << src.bytes
+            println "Converting the object to ${objectname}.zip"
+            def dst = new File("${tempDir}/${objectname}.zip") 
+            def src = new File("${env.currentworkdir}/${objectPath}")
+            dst << src.bytes
 
-             //Extracting the xdoz file in Temp Dir
-             println "Extracting the xdoz file in Temp Dir to process the object"
-             unzip dir: "${tempDir}", glob: '', zipFile: "${tempDir}/${objectname}.zip"
-             sh 'tree'
-             //dir("${tempDir}") {
-             println "Extracting the URI or absolute path from metadata.meta"
-             sh 'pwd'
-             sh 'ls -lrth'
-             metaxmlfile = new File("${tempDir}/~metadata.meta").text
-             //println "${metaxmlfile}"
-             def response = new XmlSlurper().parseText("${metaxmlfile}")
-             def cdataPath = response.entries.entry[5].value.text()
+            //Extracting the xdoz file in Temp Dir
+            println "Extracting the xdoz file in Temp Dir to process the object"
+            unzip dir: "${tempDir}", glob: '', zipFile: "${tempDir}/${objectname}.zip"
+            sh 'tree'
+            //dir("${tempDir}") {
+            println "Extracting the URI or absolute path from metadata.meta"
+            sh 'pwd'
+            sh 'ls -lrth'
+            metaxmlfile = new File("${tempDir}/~metadata.meta").text
+            //println "${metaxmlfile}"
+            def response = new XmlSlurper().parseText("${metaxmlfile}")
+            def cdataPath = response.entries.entry[5].value.text()
 
-             //             }
+            //             }
 
-             println "The Encoded URI of File : ${cdataPath}"
+            println "The Encoded URI of File : ${cdataPath}"
 
-             // Decoding the URI of file path
-             def decodedPath = URLDecoder.decode(cdataPath)
-             println "The Decoded URI of File : ${decodedPath}"
+            // Decoding the URI of file path
+            def decodedPath = URLDecoder.decode(cdataPath)
+            println "The Decoded URI of File : ${decodedPath}"
              
-             // Decoded URI without file ext
-             def decodedPathnoext = decodedPath.substring(0, decodedPath.lastIndexOf("."))
-             println "The Decoded URI of File without ext : ${decodedPathnoext}"
+            // Decoded URI without file ext
+            def decodedPathnoext = decodedPath.substring(0, decodedPath.lastIndexOf("."))
+            println "The Decoded URI of File without ext : ${decodedPathnoext}"
 
-             def encoded = src.bytes.encodeBase64()
-             //println "${encoded}"
+            def encoded = src.bytes.encodeBase64()
+            //println "${encoded}"
             
-             // Check if object exists in the destination URI using the Soap Service Call
+            // Check if object exists in the destination URI using the Soap Service Call
             
-             println "===================================================================================="
-             println " Check if Object Exists "
-             println "===================================================================================="
+            println "===================================================================================="
+            println " Check if Object Exists "
+            println "===================================================================================="
              
-             //String createTempLocation(String path) {
-             //String tmpDir = pwd tmp: true
-             //return tmpDir + File.separator + new File(path).getName()
-             //   }
-
-             def existSampledata = """
+            def existSampledata = """
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://xmlns.oracle.com/oxp/service/v2">
    <soapenv:Header/>
    <soapenv:Body>
@@ -109,42 +103,44 @@ def call () {
    </soapenv:Body>
 </soapenv:Envelope>
 """
-new File("${env.currentworkdir}/objectExists.xml").text = "${existSampledata}"
+            new File("${env.currentworkdir}/objectExists.xml").text = "${existSampledata}"
 
-             //writeFile file: "${env.currentworkdir}", text: libraryResource("objectExists.xml")
+            //writeFile file: "${env.currentworkdir}", text: libraryResource("objectExists.xml")
              
-             def param = [:] 
-             param["reportObjectAbsolutePath"] = "${decodedPath}"
-             param["userID"] = "${testuser}"
-             param["password"] = "${testpass}"
+            def param = [:] 
+            param["reportObjectAbsolutePath"] = "${decodedPath}"
+            param["userID"] = "${testuser}"
+            param["password"] = "${testpass}"
 
-             objectExistsFile = new File("${env.currentworkdir}/objectExists.xml").text
-             //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
-             def objectExistsdata = new XmlSlurper().parseText("${objectExistsFile}")
-             param.each { key,value ->
-             // change the node value if the its name matches
-             objectExistsdata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
+            objectExistsFile = new File("${env.currentworkdir}/objectExists.xml").text
+            //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
+            def objectExistsdata = new XmlSlurper().parseText("${objectExistsFile}")
+            param.each { key,value ->
+            // change the node value if the its name matches
+            objectExistsdata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
                          }
-             def checkXml = XmlUtil.serialize(objectExistsdata)
+            def checkXml = XmlUtil.serialize(objectExistsdata)
             
-             def newXml = new File("${env.currentworkdir}/check.xml")
-             newXml.write("${checkXml}")
-             println "${checkXml}"
+            def newXml = new File("${env.currentworkdir}/check.xml")
+            newXml.write("${checkXml}")
+            println "${checkXml}"
             
-             //Create Soap Call
+            //Create Soap Call
 
-             //case1()
+            //case1()
 
-             // If object Exists in the URI use update SOAP Call
-             env.condition = "false"
+            // If object Exists in the URI use update SOAP Call
+            
 
-             if(env.condition == "false") {
+            env.condition = "false"
 
-             println "===================================================================================="
-             println "The Object Already Exists so executing the update SOAP CALL"
-             println "===================================================================================="
+            if(env.condition == "true") {
+
+            println "===================================================================================="
+            println "The Object Already Exists so executing the update SOAP CALL"
+            println "===================================================================================="
              
-             def updateSampledata = """
+            def updateSampledata = """
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://xmlns.oracle.com/oxp/service/v2">
    <soapenv:Header/>
    <soapenv:Body>
@@ -157,36 +153,36 @@ new File("${env.currentworkdir}/objectExists.xml").text = "${existSampledata}"
    </soapenv:Body>
 </soapenv:Envelope>
 """
-             new File("${env.currentworkdir}/updateObject.xml").text = "${updateSampledata}"
+            new File("${env.currentworkdir}/updateObject.xml").text = "${updateSampledata}"
 
-             def param1 = [:] 
-             param1["objectAbsolutePath"] = "${decodedPath}"
-             param1["objectData"] = "${encoded}"
-             param1["userID"] = "${testuser}"
-             param1["password"] = "${testpass}"
+            def param1 = [:] 
+            param1["objectAbsolutePath"] = "${decodedPath}"
+            param1["objectData"] = "${encoded}"
+            param1["userID"] = "${testuser}"
+            param1["password"] = "${testpass}"
              
-             updatexmlFile = new File("${env.currentworkdir}/updateObject.xml").text
-             //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
-             def updatexmldata = new XmlSlurper().parseText("${updatexmlFile}")
-             param1.each { key,value ->
-             // change the node value if the its name matches
-             updatexmldata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
+            updatexmlFile = new File("${env.currentworkdir}/updateObject.xml").text
+            //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
+            def updatexmldata = new XmlSlurper().parseText("${updatexmlFile}")
+            param1.each { key,value ->
+            // change the node value if the its name matches
+            updatexmldata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
                          }
-             def updateXml = XmlUtil.serialize(updatexmldata)
+            def updateXml = XmlUtil.serialize(updatexmldata)
             
-             def newupdateXml = new File("${env.currentworkdir}/update.xml")
-             newupdateXml.write("${updateXml}")
-             println "${updateXml}"
+            def newupdateXml = new File("${env.currentworkdir}/update.xml")
+            newupdateXml.write("${updateXml}")
+            println "${updateXml}"
             
-             // Create Update Soap Call
+            // Create Update Soap Call
 
-             } else if(condition == "false"){
+            } else if(condition == "false"){
             
-             println "===================================================================================="
-             println "The Object doesn't Exists so executing the upload SOAP CALL"
-             println "===================================================================================="
+            println "===================================================================================="
+            println "The Object doesn't Exists so executing the upload SOAP CALL"
+            println "===================================================================================="
             
-             def mainSampledata = """
+            def mainSampledata = """
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://xmlns.oracle.com/oxp/service/v2">
    <soapenv:Header/>
    <soapenv:Body>
@@ -201,34 +197,34 @@ new File("${env.currentworkdir}/objectExists.xml").text = "${existSampledata}"
 </soapenv:Envelope>
 """
 
-             new File("${env.currentworkdir}/main.xml").text = "${mainSampledata}"
+            new File("${env.currentworkdir}/main.xml").text = "${mainSampledata}"
 
-             def param2 = [:] 
-             param2["reportObjectAbsolutePathURL"] = "${decodedPathnoext}"
-             param2["objectType"] = "${extFile}"
-             param2["objectZippedData"] = "${encoded}"
-             param2["userID"] = "${testuser}"
-             param2["password"] = "${testpass}"
+            def param2 = [:] 
+            param2["reportObjectAbsolutePathURL"] = "${decodedPathnoext}"
+            param2["objectType"] = "${extFile}"
+            param2["objectZippedData"] = "${encoded}"
+            param2["userID"] = "${testuser}"
+            param2["password"] = "${testpass}"
 
-             uploadxmlFile = new File("${env.currentworkdir}/main.xml").text
-             //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
-             def uploadxmldata = new XmlSlurper().parseText("${uploadxmlFile}")
-             param2.each { key,value ->
-             // change the node value if the its name matches
-             uploadxmldata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
+            uploadxmlFile = new File("${env.currentworkdir}/main.xml").text
+            //def objectExistsFile = getClass().getResourceAsStream("objectExists.xml")
+            def uploadxmldata = new XmlSlurper().parseText("${uploadxmlFile}")
+            param2.each { key,value ->
+            // change the node value if the its name matches
+            uploadxmldata.'**'.findAll { if(it.name() == key ) it.replaceBody value }
                          }
-             def uploadXml = XmlUtil.serialize(uploadxmldata)
+            def uploadXml = XmlUtil.serialize(uploadxmldata)
             
-             def newuploadXml = new File("${env.currentworkdir}/upload.xml")
-             newuploadXml.write("${uploadXml}")
-             println "${uploadXml}"
+            def newuploadXml = new File("${env.currentworkdir}/upload.xml")
+            newuploadXml.write("${uploadXml}")
+            println "${uploadXml}"
             
-             // Create Upload Soap Call
+            // Create Upload Soap Call
 
-             } else {
-             println "========================================================================================="
-             println "The Object Exists Soap Call resulted in Error. Please Check logs or verify the objectlist"
-             println "========================================================================================="
+            } else {
+            println "========================================================================================="
+            println "The Object Exists Soap Call resulted in Error. Please Check logs or verify the objectlist"
+            println "========================================================================================="
 
                          } 
                     }
--- vars/test.groovy
+++ vars/test.groovy
@@ -5,61 +5,76 @@
 //def objectslist_list  = objectlist.collect { it }
 //println "${objectslist_list}"
 
-String objectlist = "C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt"
-File objectlistfile = new File (objectlist)
-def objectslist_list  = objectlistfile.collect { it }
+// String objectlist = "C:/Users/Yatin/Desktop/github/OracleTest/objectlist.txt"
+// File objectlistfile = new File (objectlist)
+// def objectslist_list  = objectlistfile.collect { it }
 
 
-for(String j in objectslist_list) {
-//println "${j}"
-File f = new File(j)
-def objectnameext = f.getName()
-def extension = objectnameext.substring(objectnameext.lastIndexOf('.') + 1)
-//println "${extension}"
+// for(String j in objectslist_list) {
+// //println "${j}"
+// File f = new File(j)
+// def objectnameext = f.getName()
+// def extension = objectnameext.substring(objectnameext.lastIndexOf('.') + 1)
+// //println "${extension}"
 
-if ( ("${extension}" == "xdoz") || ("${extension}" == "xdmz") ) {
-    File reportsFile = new File("objectlist_reports.txt")
-    //reportsfile.write "${j}"
-    reportsFile.append("${j}\n")
-   // println reportsFile.text
-}
-else if ("${extension}" == "iar") {
-    File oicFile = new File("objectlist_oic.txt")
-    //reportsfile.write "${j}"
-    oicFile.append("${j}\n")
-    //println oicFile.text
-}
+// if ( ("${extension}" == "xdoz") || ("${extension}" == "xdmz") ) {
+//     File reportsFile = new File("objectlist_reports.txt")
+//     //reportsfile.write "${j}"
+//     reportsFile.append("${j}\n")
+//    // println reportsFile.text
+// }
+// else if ("${extension}" == "iar") {
+//     File oicFile = new File("objectlist_oic.txt")
+//     //reportsfile.write "${j}"
+//     oicFile.append("${j}\n")
+//     //println oicFile.text
+// }
 
-}
-reportflag = new File('objectlist_reports.txt').exists()
-println "${reportflag}"
-oicflag = new File('objectlist_oic.txt').exists()
-println "${oicflag}"
-//def bothflag =  (new File('objectlist_oic.txt').exists() && new File('objectlist_oic.txt').exists())
-if (("${reportflag}" == "true") && ("${oicflag}" == "false")) {
-    println "report file exists"
-     num = 1;
-} else if (("${reportflag}" == "false") && ("${oicflag}" == "true")) {
-    println "oic file exists"
-    num = 2;
-} else if (("${reportflag}" == "true") && ("${oicflag}" == "true")) {
-    println "both file exists"
-    num = 3;
-} else {
-    println "Only files with ext xdoz, xdmz & iar are allowed in this pipeline"
-}
+// }
+// reportflag = new File('objectlist_reports.txt').exists()
+// println "${reportflag}"
+// oicflag = new File('objectlist_oic.txt').exists()
+// println "${oicflag}"
+// //def bothflag =  (new File('objectlist_oic.txt').exists() && new File('objectlist_oic.txt').exists())
+// if (("${reportflag}" == "true") && ("${oicflag}" == "false")) {
+//     println "report file exists"
+//      num = 1;
+// } else if (("${reportflag}" == "false") && ("${oicflag}" == "true")) {
+//     println "oic file exists"
+//     num = 2;
+// } else if (("${reportflag}" == "true") && ("${oicflag}" == "true")) {
+//     println "both file exists"
+//     num = 3;
+// } else {
+//     println "Only files with ext xdoz, xdmz & iar are allowed in this pipeline"
+// }
 
-switch(num) {
-    case 1:
-    case 3:
-        println ("Value of Case is 1");
-        initialprocess();
-        if (num == 3){
-         println ("Go to case 2")   
-        }else {
-        break;
-        }
-    case 2:
-        println ("Value of Case is 2");
-        break; 
-}
+// switch(num) {
+//     case 1:
+//     case 3:
+//         println ("Value of Case is 1");
+//         initialprocess();
+//         if (num == 3){
+//          println ("Go to case 2")   
+//         }else {
+//         break;
+//         }
+//     case 2:
+//         println ("Value of Case is 2");
+//         break; 
+// }
+
+
+def output = """
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<soapenv:Body>
+<objectExistResponse xmlns="http://xmlns.oracle.com/oxp/service/v2">
+<objectExistReturn>false</objectExistReturn>
+</objectExistResponse>
+</soapenv:Body>
+</soapenv:Envelope>
+"""
+
+def response = new XmlSlurper().parseText(output)
+def Result = response.Envelope.Body.objectExistResponse[0].objectExistReturn
+println "${Result}"
